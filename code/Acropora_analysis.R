18eee6e3818bd76c2bab40ec8a02cfcc55814d34
mfischer-edi <s1742913@sms.ed.ac.uk>
2022-10-24 15:58:29 +0100

    Mara ran some LMMs and made more plots

--- code/Acropora_analysis.R
+++ code/Acropora_analysis.R
@@ -11,6 +11,9 @@ library(tidyr)
 library(ggplot2)
 library(cowplot)
 library(forcats)
+library(lme4)
+library(lmerTest)
+library(DHARMa)
 
 # Create theme for plots
 theme_diss <- function(){            
@@ -60,11 +63,71 @@ mean(perc_all22$Acropora) + mean(perc_all22$Coral) # 27.49366
 # Percentage of non-acropora out of total:
 22.60464/27.49366 * 100  # 82.21765 %
 
-# Add column for total cover
-perc_all22 <- perc_all22 %>% 
-  mutate(Total.coral = Acropora + Coral)
+# Data manipulation raw data ----
 
-# Calculate mean, sd and SE for both
+# Factor depth
+perc_all22$Depth <- factor(perc_all22$Depth, levels = c("5m", "10m", "15m"),
+                           labels = c("5m", "10m", "15m"))
+
+# Add column for total cover & Remove unneeded columns & get column for rep
+perc_all22 <- perc_all22 %>% 
+  mutate(Total.coral = Acropora + Coral) %>% 
+  select(- Tape, - Wand, - Shadow, - Sum.excl.TWS) %>% 
+  separate(Photo.Name, into = c("S", "D", "Rep", "Photo"), sep = "_")
+
+# Check Rep column:
+unique(perc_all22$Rep) # 2 reps
+
+# Remove unneeded columns
+perc_all22 <- perc_all22 %>% 
+  select(- S, - D, - Photo)
+
+## Aggregate at replicate level ----
+perc_rep <- perc_all22 %>% 
+  group_by(Site.name, Depth, Rep) %>% 
+  summarise(Reef.type = unique(Reef.type), 
+            Acropora = mean(Acropora), Other.coral = mean(Coral),
+            Total.coral = mean(Total.coral), 
+            Bleached.Acropora = mean(Bleached.Acropora),
+            Bleached.other.coral = mean(Bleached.Coral),
+            Total.bleached = mean(Bleached.Acropora) + mean(Bleached.Coral)) %>% 
+  ungroup()
+
+# Add column for total coral incl. bleached
+
+perc_rep <- perc_rep %>% 
+  mutate(Total.incl.bleached = Total.coral + Total.bleached)
+
+# Check averages still match up
+mean(perc_rep$Acropora)
+mean(perc_rep$Other.coral)
+mean(perc_rep$Total.coral)
+## all the same as before!
+
+# Laamu-wide averages for bleached coral
+mean(perc_rep$Bleached.Acropora)  # 0.695625
+mean(perc_rep$Bleached.other.coral)  # 0.07392857
+mean(perc_rep$Total.bleached)  # 0.7695536
+
+# Percentage of acropora out of total bleached:
+0.695625/0.7695536 * 100  # 90.39331 %
+
+# Percentage of non-acropora out of total bleached:
+0.07392857/0.7695536 * 100   # 9.606682 %
+
+# Laamu-wide average for total coral incl. bleached
+mean(perc_rep$Total.incl.bleached)  # 28.26321
+
+# Percentage of bleached coral out of total:
+0.7695536/28.26321 * 100   # 2.72281 %
+
+# Percentage of live coral out of total:
+27.49366/28.26321 * 100   # 97.2772 %
+
+## Calculate mean cover + SE ----
+
+# From raw data:
+# Calculate mean, sd and SE for both Acropora and other coral
 mean_cover <- perc_all22 %>% 
   summarise(Acropora.mean = mean(Acropora), 
             Acropora.sd = sd(Acropora),
@@ -77,6 +140,34 @@ mean_cover <- perc_all22 %>%
             Total.sd = sd(Total.coral),
             Total.SE = sd(Total.coral)/sqrt(n)) %>% 
   ungroup()
+
+# From data aggregated by replicate:
+mean_cover2 <- perc_rep %>% 
+  summarise(Acropora.mean = mean(Acropora), 
+            Acropora.sd = sd(Acropora),
+            n = n(),
+            Acropora.SE = sd(Acropora)/sqrt(n()),
+            Coral.mean = mean(Other.coral), 
+            Coral.sd = sd(Other.coral),
+            Coral.SE = sd(Other.coral)/sqrt(n()),
+            Total.mean = mean(Total.coral),
+            Total.sd = sd(Total.coral),
+            Total.SE = sd(Total.coral)/sqrt(n),
+            BAcropora.mean = mean(Bleached.Acropora), 
+            BAcropora.sd = sd(Bleached.Acropora),
+            BAcropora.SE = sd(Bleached.Acropora)/sqrt(n()),
+            BCoral.mean = mean(Bleached.other.coral), 
+            BCoral.sd = sd(Bleached.other.coral),
+            BCoral.SE = sd(Bleached.other.coral)/sqrt(n()),
+            TotalB.mean = mean(Total.bleached), 
+            TotalB.sd = sd(Total.bleached),
+            TotalB.SE = sd(Total.bleached)/sqrt(n()),
+            BTotal.mean = mean(Total.incl.bleached),
+            BTotal.sd = sd(Total.incl.bleached),
+            BTotal.SE = sd(Total.incl.bleached)/sqrt(n)) %>% 
+    ungroup()
+  
+## SEs slightly different now as different sample size
 
 # Data exploration with data aggr. by depth ----
 
@@ -99,16 +190,10 @@ mean(all22sites$Total.hard.coral)  # 27.49366
 
 ### All the same averages, so will use full raw dataset for now ###
 
-perc_all22$Depth <- factor(perc_all22$Depth, levels = c("5m", "10m", "15m"),
-                           labels = c("5m", "10m", "15m"))
 
 # Visualisation ----
 
-# Stacked barplot
-
-# Remove unneeded columns:
-perc_all22 <- perc_all22 %>% 
-  select(- Tape, - Wand, - Shadow, - Sum.excl.TWS)
+## Stacked barplots ----
 
 perc_long <- perc_all22 %>% 
   pivot_longer(cols = 7:25, names_to = "Cover", values_to = "Percent")
@@ -283,7 +368,7 @@ coral_long %>%
         plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
 
 
-# Pie chart of Laamu wide average
+## Pie chart of Laamu wide average ----
 
 # Make subset with averaged proportions
 coral_prop <- coral_long %>% 
@@ -298,4 +383,80 @@ pie(coral_prop$Prop, labels = c("Acropora", "Other coral"))
 # With percentage out of total
 pie(coral_prop$Perc, labels = c("Acropora (17.8%)", "Other coral (82.2%)"), 
     col = c("#fcd0a1", "#b1b695"), border = "white")
+
+## Visualise bleached out of total coral ----
+
+# Remove total columns for visualisation processes
+perc_rep_long <- perc_rep %>% 
+  select(- Total.coral, - Total.bleached, - Total.incl.bleached)
+
+perc_rep_long <- perc_rep_long %>% 
+  pivot_longer(cols = 5:8, names_to = "Cover", values_to = "Percent")
+
+# Add column to use as x-axis to show Laamu-wide average
+perc_rep_long <- perc_rep_long %>% 
+  mutate(X = "Laamu")
+
+# Reorder categories
+perc_rep_long$Cover <- factor(perc_rep_long$Cover,
+                               levels = c("Acropora", "Bleached.Acropora",
+                                          "Other.coral", "Bleached.other.coral"),
+                               labels = c("Acropora", "Bleached.Acropora",
+                                          "Other.coral", "Bleached.other.coral"))
+
+
+(coral_stack <- ggplot(perc_rep_long, aes(x = X, y = Percent, fill = Cover)) +
+    geom_bar(position = "fill", stat = "identity", width = 0.5) +
+    scale_y_continuous(labels = scales::percent,
+                       expand = c(0,0)) +
+    scale_colour_manual(values = c("#cc8b86", "#f9eae1", "#7d4f50", "#d1be9c"),
+                        labels = c("Acropora", "Bleached Acropora",
+                                   "Other coral", "Bleached other coral")) +
+    scale_fill_manual(values = c("#cc8b86", "#f9eae1", "#7d4f50", "#d1be9c"),
+                      labels = c("Acropora", "Bleached Acropora",
+                                 "Other coral", "Bleached other coral")) +
+    labs(y = "Percent cover") +
+    theme_diss() +
+    theme(legend.position = "bottom",
+          legend.title = element_blank(),
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          panel.border = element_rect(fill = NA),
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
+)
+
+# Same as pie chart
+
+# Make subset with averaged proportions
+coral_prop <- perc_rep_long %>% 
+  group_by(Cover) %>% 
+  summarise(Prop = mean(Percent)) %>% 
+  ungroup()
+
+pie(coral_prop$Prop, labels = c("Acropora", "Bleached Acropora",
+                                "Other coral", "Bleached other coral"),
+    col = c("#cc8b86", "#f9eae1", "#7d4f50", "#d1be9c"), border = "white")
+
+# Analysis ----
+
+## Effect of depth on Acropora cover ----
+
+hist(perc_rep$Acropora)
+
+m1 <- lmer(Acropora ~ Depth + (1|Site.name), data = perc_rep)
+summary(m1)
+plot(m1)
+simulateResiduals(m1, plot = T)  # red!
+anova(m1)  # n.s
+
+## Effect of reef type on Acropora cover ----
+
+m2 <- lmer(Acropora ~ Reef.type + (1|Site.name), data = perc_rep)
+summary(m2)
+plot(m2)
+simulateResiduals(m2, plot = T)  # red! (homogeneity of variance sig.)
+anova(m2)  # n.s
+
+
+
 
