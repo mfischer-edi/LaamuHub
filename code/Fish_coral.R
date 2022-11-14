97d42cc509038d5c42a3fe2c53a193c91ccd8cfd
mfischer-edi <s1742913@sms.ed.ac.uk>
2022-11-14 17:49:33 +0000

    Mara tidied messy fish data

--- code/Fish_coral.R
+++ code/Fish_coral.R
@@ -83,3 +83,60 @@ fish_all22 <- fish22 %>%
 
 sum(fish_all22$abundance)
 sum(fish_all22$biomass)
+
+## 2022 all raw fish data ----
+
+raw_fish22 <- read.csv("data/raw_fish_2022.csv", header = T)
+
+# Check data
+unique(raw_fish22$Site) # all 29 sites
+unique(raw_fish22$Reef.type) # the 4 reef types
+unique(raw_fish22$Depth)  # the 3 depths
+unique(raw_fish22$Replicate.No.) # 2 reps
+unique(raw_fish22$Family)  # 9 families
+unique(raw_fish22$Species) # 106 species
+
+# Aggregate to get abundance and biomass in same data frame
+agg_fish22 <- raw_fish22 %>% 
+  group_by(Site, Depth, Replicate.No., Family, Species, Biomass) %>% 
+  summarise(abundance = length(Species),
+            Site.name = unique(Site.name)) %>% 
+  ungroup()
+## now have the abundance of a species of a given biomass at each rep
+
+# Create dataset with fish abundance without biomass
+abun_fish22 <- raw_fish22 %>% 
+  group_by(Site, Depth, Replicate.No., Family, Species) %>% 
+  summarise(abundance = length(Species),
+            Site.name = unique(Site.name),
+            reef.type = unique(Reef.type)) %>% 
+  ungroup()
+
+write.csv(abun_fish22, "data/abun_rep_sp_2022.csv")
+
+# Create one ignoring depth and rep
+abun_site22 <- raw_fish22 %>% 
+  group_by(Site, Family, Species) %>% 
+  summarise(count = length(Species),
+            Site.name = unique(Site.name),
+            reef.type = unique(Reef.type),
+            year = "2021/22") %>% 
+  ungroup()
+
+write.csv(abun_site22, "data/abun_site_sp_2022.csv")
+
+# Dataset for overall abundance & biomass (ignoring species)
+abun_site_agg22 <- raw_fish22 %>% 
+  group_by(Site, Depth, Replicate.No.) %>% 
+  summarise(abundance = length(Species),
+            Site.name = unique(Site.name),
+            reef.type = unique(Reef.type),
+            year = "2021/22") %>% 
+  ungroup()
+
+write.csv(abun_site_agg22, "data/abun_site_2022.csv")
+
+
+
+
+
